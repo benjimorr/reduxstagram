a406ed8768da608ea177da3f35a90e669b167e2c
Ben Morrison <benmorrison0@gmail.com>
2018-05-07 14:15:34 -0400

    Added functionality to add and remove comments on individual photos

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,4 +1,29 @@
+function postComments(state = [], action) {
+	switch(action.type) {
+		case 'ADD_COMMENT':
+			return [...state, {
+				user: action.author,
+				text: action.comment
+			}];
+		case 'REMOVE_COMMENT':
+			const { postId, index } = action;
+
+			return [
+				...state.slice(0, index),
+				...state.slice(index + 1)
+			];
+		default:
+			return state;
+	}
+}
+
 function comments(state = [], action) {
+	if(typeof action.postId !== 'undefined') {
+		return {
+			...state,
+			[action.postId]: postComments(state[action.postId], action)
+		}
+	}
 	return state;
 }
 
